900e3d3cf18cd8c9b32ef18c58c3c5ae9cc17561
zhangkaiyuan <1036254540@qq.com>
2025-04-28 14:11:38 +0800

    wx-sdk

---
+++ src/main/java/com/zky/chatgpt/interfaces/WeiXinPortalController.java
@@ -0,0 +1,14 @@
+package com.zky.chatgpt.interfaces;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: ZhangKaiYuan
+ * @description: 微信公众号，请求处理服务
+ * @create: 2025/4/28
+ */
+@RestController
+@RequestMapping("/wx/portal/{appid}")
+public class WeiXinPortalController {
+}
